7887b5753309dc1b976ec03d50599cc54f52d1d6
Kunal More <morekunal635@gmail.com>
2021-02-01 13:45:56 +0530

    Final commit

--- js/app.js
+++ js/app.js
@@ -9,10 +9,10 @@ const coffees = [
   { name: "Inventore", image: "images/coffee7.jpg" },
   { name: "Veritatis", image: "images/coffee8.jpg" },
   { name: "Accusantium", image: "images/coffee9.jpg" },
-]
+];
 
 const showCoffees = () => {
-    let output = ""
+    let output = "";
     coffees.forEach(
       ({ name, image }) =>
         (output += `
@@ -22,17 +22,17 @@ const showCoffees = () => {
                   <a class="card--link" href="#">Taste</a>
                 </div>
                 `)
-    )
-    container.innerHTML = output
-}
+    );
+    container.innerHTML = output;
+};
   
-document.addEventListener("DOMContentLoaded", showCoffees)
+document.addEventListener("DOMContentLoaded", showCoffees);
 
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
         navigator.serviceWorker
         .register("serviceWorker.js")
         .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
+        .catch(err => console.log("service worker not registered", err));
+    });
 }
--- serviceWorker.js
+++ serviceWorker.js
@@ -13,20 +13,20 @@ const assets = [
     "/images/coffee7",
     "/images/coffee8",
     "/images/coffee9",
-]
+];
 
 self.addEventListener("install", installEvent => {
     installEvent.waitUntil(
         caches.open(staticMoreCoffee).then(cache => {
             cache.addAll(assets)
         })
-    )
-})
+    );
+});
 
 self.addEventListener("fetch", fetchEvent => {
     fetchEvent.respondWith(
       caches.match(fetchEvent.request).then(res => {
         return res || fetch(fetchEvent.request)
       })
-    )
-  })
+    );
+});
